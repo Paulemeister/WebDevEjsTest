a08f062aef0211bd96a0a356b613cfe7572b2040
Paulemeister <annanas6800i@gmail.com>
2021-02-16 14:00:07 +0100

    super messy test

--- server.js
+++ server.js
@@ -2,53 +2,97 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const marked = require("marked");
-const path = require("path")
+const path = require("path");
+const qs = require("querystring");
+const ejs = require("ejs");
 
 //mysql = require("./mysql");
 config = require("./config.js");
 
+const paths = {
+    "/": "/index.html",
+    "/form": "/form.html",
+    "/md": "/test.md"
+}
+
 const requestListener = function (request, response) {
-    let pathname = url.parse(request.url).pathname;
+    console.log(request.url);
+    let url = new URL(request.url,"http://127.0.0.1:8080");
+    let pathname = url.pathname;
     console.log("Requested:", pathname);
     console.log("Type: ", request.method)
 
     if (request.method == "GET") {
 
-        if (pathname == "/") {
-            pathname = "/index.html";
-        } else if (pathname == "/form") {
-            pathname = "/form.html"
-        } else if (pathname == "/md") {
-            pathname = "/test.md"
+        if (pathname in paths) {
+            pathname = paths[pathname];
         }
         //console.log(request);
         console.log("Serving:", pathname.substring(1))
-        let extension = path.extname(pathname.substring(1));
-        
-            fs.readFile(pathname.substring(1), (error, data) => {
-                if (error) {
-                    console.error(error);
-                    response.writeHead(404, { 'Content-Type': 'text/plain; charset=UTF-8' });
-                    response.write('404 - File Not Found :(');
-                } else {
-                    response.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
-                    
-                    console.log("Extension",extension);
-                    if (extension == ".md") {
-                        response.write(marked(data.toString()));
-                    } else {  
-                        response.write(data.toString());
-                    }
-                    
-                    console.log("Success");
-                }
+
+        if (!fs.existsSync("./rendered/"+pathname.substring(1))){
+            renderPage(pathname.substring(1));
+        }
+        fs.readFile("./rendered/"+pathname.substring(1), (error, data) => {
+            if (error) {
+                
+                console.error(error);
+                response.writeHead(404, { 'Content-Type': 'text/plain; charset=UTF-8' });
+                response.write('404 - File Not Found :(');
                 response.end();
-            })
-        
+            }
+            else {
+    
+                response.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+
+                response.write(data.toString());
+                
+                console.log("Success");
+            }
+            response.end();
+            
+        })
+
     }
     else if (request.method == "POST") {
-        console.log(request);
+        //1
+        request.on("data",function(data) {
+            //4
+            let result = qs.parse(data.toString());
+            console.log(result);
+        });
+        //2
+        request.on("end",function(){
+            //5
+            response.end();
+        });
+        //3
+
     }
+    //never executes
+}
+
+function renderPage(pathname) {
+    let html = "This Page is empty. Maybe it didn't render?";
+    let name =pathname.split(".")[0];
+    console.log("rendering ./sites/"+name +".ejs")
+    try {
+        let contents =fs.readFileSync("./sites/"+name +".ejs");
+        html = ejs.render(contents.toString());
+
+    }
+    catch (error) {
+        console.error(error);
+        return false;
+    }
+
+    fs.writeFile("./rendered/"+name+".html",html,(error)=>{
+        if (error){
+            console.error(error);
+            return false;
+        }
+    });
+    return true;
 }
 
 const server = http.createServer(requestListener);
